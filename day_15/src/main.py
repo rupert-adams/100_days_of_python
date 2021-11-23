c2c6ce67fff56148e0e0bdfb1d3807e79d1cbeae
Rupert Adams <RupertNCAdams@aol.com>
2021-11-23 17:26:20 +0100

    completed step 3: coin process

--- day_15/src/main.py
+++ day_15/src/main.py
@@ -2,6 +2,7 @@
 # Coffee data provided although I chose to use json for file. everything else is the challenge.
 
 import json
+from decimal import *
 
 machine_f, coffee_f =  open('src/data/machine.json'), open('src/data/coffee.json')
 machine_data, coffee_data = json.load(machine_f), json.load(coffee_f)
@@ -26,12 +27,32 @@ def check_levels(choice: str, machine_data: dict = machine_data["machine"], coff
         return False
 
 # 3. process coins, ask for quantity
+def coin_process(coin_list: list, coffee_price: int, coins_accepted: list = machine_data["accepted coins"]):
+    for item in coin_list:
+        if item not in coins_accepted:
+            print(f"£{item} coin not accepted, please use one of the following coins:{coins_accepted}")
+            print(f"refunding £{item} coin")
+            return False
+    total_coins = sum(coin_list)
+    refund_value = 0
+    if total_coins >= coffee_price:
+        if total_coins > coffee_price:
+            refund_value = total_coins-coffee_price
+            print(f"paid too much, refunding £{'{0:.2f}'.format(refund_value)}")
+        return True
+    else:
+        refund_value = total_coins
+        print(f"Not enough coins, you paid £{'{0:.2f}'.format(refund_value)}, £{'{0:.2f}'.format(coffee_price)} is needed, refunding your money")
+        return False
+
+
 
 # 4. give refund if not enough or too much
 
 # 5. make coffee
 
-if check_levels("espresso"):
-    print("Hurray!")
+# if check_levels("espresso"):
+#     print("Hurray!")
+coin_process([1,2,1], 3)
 machine_f.close()
 coffee_f.close()
--- day_15/test/test_main.py
+++ day_15/test/test_main.py
@@ -1,8 +1,8 @@
-from src.main import report, check_levels
+from src.main import report, check_levels, coin_process
 
 EXPECTED_REPORT = "Report:\nwater: 300ml\nmilk: 200ml\ncoffee: 100mg"
-EXPECTED_LEVELS_FAILED = False
-EXPECTED_LEVELS_SUCCEEDED = True
+EXPECTED_FAILURE = False
+EXPECTED_SUCCESS = True
 
 def test_report(machine_data_block):
     result = report(machine_data_block["machine"])
@@ -11,9 +11,24 @@ def test_report(machine_data_block):
 def test_check_levels_failed(coffee_data_block, machine_data_block):
     coffee = "extreme_test_coffee"
     result = check_levels(coffee, machine_data_block["machine"], coffee_data_block)
-    assert result == EXPECTED_LEVELS_FAILED
+    assert result == EXPECTED_FAILURE
 
 def test_check_levels_succeeded(coffee_data_block, machine_data_block):
     coffee = "cheap_test_coffee"
     result = check_levels(coffee, machine_data_block["machine"], coffee_data_block)
-    assert result == EXPECTED_LEVELS_SUCCEEDED
+    assert result == EXPECTED_SUCCESS
+
+def test_coin_process_too_little(coffee_data_block, machine_data_block):
+    test_coin_list = [1, 0.50]
+    result = coin_process(test_coin_list, coffee_data_block["extreme_test_coffee"]["price"], machine_data_block["accepted coins"])
+    assert result == EXPECTED_FAILURE
+
+def test_coin_process_wrong_coin(coffee_data_block, machine_data_block):
+    test_coin_list = [1, 0.50, 0.25]
+    result = coin_process(test_coin_list, coffee_data_block["extreme_test_coffee"]["price"], machine_data_block["accepted coins"])
+    assert result == EXPECTED_FAILURE
+
+def test_coin_process_just_right(coffee_data_block, machine_data_block):
+    test_coin_list = [0.50, 0.50]
+    result = coin_process(test_coin_list, coffee_data_block["cheap_test_coffee"]["price"], machine_data_block["accepted coins"])
+    assert result == EXPECTED_SUCCESS
