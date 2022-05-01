ca8c3dec79369fdee266310251f62578e2a7a0e1
Rupert Adams <RupertNCAdams@aol.com>
2022-05-01 11:59:11 +0200

    adding etch_a_sketch

---
+++ day_19/etch_a_sketch.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+curser = Turtle()
+screen = Screen()
+
+def move_forward():
+    curser.forward(10)
+
+def move_backward():
+    curser.backward(10)
+
+def turn_clockwise():
+    curser.right(10)
+
+def turn_counterclockwise():
+    curser.left(10)
+
+def reset():
+    curser.clear()
+    curser.home()
+
+INPUT_CONSTRAINT= {
+    "w": move_forward,
+    "s": move_backward,
+    "a": turn_counterclockwise,
+    "d": turn_clockwise,
+    "c": reset,
+}
+
+def locomotion():
+    for item in INPUT_CONSTRAINT:
+        screen.onkey(key=item, fun=INPUT_CONSTRAINT[item])
+
+screen.listen()
+locomotion()
+screen.exitonclick()
