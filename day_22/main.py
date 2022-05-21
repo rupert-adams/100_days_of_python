7c2a90723a845e92927130d16b06f299dac07b2d
Rupert Adams <RupertNCAdams@aol.com>
2022-05-21 16:09:19 +0200

    added paddle class

--- day_22/main.py
+++ day_22/main.py
@@ -1,8 +1,27 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+# Create a constraint and for loop
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+
+
+game_on = True
+
+while game_on:
+    screen.update()
 
 screen.exitonclick()
