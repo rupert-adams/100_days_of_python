70d0f46afda5f00b3e3622a16dea7d458f283d86
Rupert Adams <RupertNCAdams@aol.com>
2022-05-21 16:46:05 +0200

    adding ball functionality

--- day_22/main.py
+++ day_22/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 # Create a constraint and for loop
@@ -22,6 +25,24 @@ screen.onkey(l_paddle.go_down,"s")
 game_on = True
 
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+    
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
